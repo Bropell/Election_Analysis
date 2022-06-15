ad0f3df1295dbb0b4bf22c996bd78330109c7eaa
Brandon Ropell <brandon.ropell@gmail.com>
2022-06-15 00:14:36 -0400

    Added While_Loop.py and updated practice

--- Python_practice.py
+++ Python_practice.py
@@ -26,3 +26,28 @@ else:
 
     print("Arapahoe or El Paso is not in the list of counties.")
 
+
+for county in range(len(counties)):
+
+    print(counties[county])
+
+
+
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}   
+
+for county, voters in counties_dict.items():
+    print(f"{county} county has {voters} registered voters.")
+
+
+
+
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+                {"county":"Denver", "registered_voters": 463353},
+                {"county":"Jefferson", "registered_voters": 432438}]
+
+
+for county_dict in voting_data:
+    
+    for value in county_dict.values():
+
+        print(value)
