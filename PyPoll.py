2ec5fe6f39de900f1605f35d5cf774d237364db8
Brandon Ropell <brandon.ropell@gmail.com>
2022-06-18 15:40:37 -0400

    Updated PyPoll file path

--- PyPoll.py
+++ PyPoll.py
@@ -12,7 +12,7 @@ file_to_load = os.path.join("Resources", "election_results.csv")
 
 # Assign a filename variable to save the file to a path.
 
-file_to_save = os.path.join("analysis", "election_analysis.txt")
+file_to_save = os.path.join("analysis", "election_results.txt")
 
 
 
